2b4a99002dafac1c1fb2ae25ca1c7bc10fc5de5d
shaunokeefe <shaun.okeefe.0@gmail.com>
2013-12-01 21:14:57 +1100

    Add models to admin site

--- hoponit/venues/admin.py
+++ hoponit/venues/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Venue)
+admin.site.register(models.Address)
+admin.site.register(models.Beer)
+admin.site.register(models.BeerType)
