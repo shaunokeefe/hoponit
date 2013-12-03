5df8dcbe6baf696a081501456cc5ff2c9266b197
shaunokeefe <shaun.okeefe.0@gmail.com>
2013-12-04 09:03:30 +1100

    add view to find venues in suburb

--- hoponit/venues/views.py
+++ hoponit/venues/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Venue
+
+
+class VenueBySuburbList(ListView):
+    model = Venue
+    context_object_name = 'venues'
+
+    def get_queryset(self):
+        suburb_name = self.request.GET.get('suburb_name', None)
+        return Venue.objects.filter(address__suburb=suburb_name)
+
+    def get_context_data(self, **kwargs):
+        context = super(VenueBySuburbList, self).get_context_data(**kwargs)
+        context['suburb_name'] = self.request.GET.get('suburb_name', None)
+        return context
