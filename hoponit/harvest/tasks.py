780dda565d9ad6a75d53b09311741f98768efae8
shaunokeefe <shaun.okeefe.0@gmail.com>
2014-03-14 22:29:36 +1100

    store and display unique beers for venue/suburb

--- hoponit/harvest/tasks.py
+++ hoponit/harvest/tasks.py
@@ -19,13 +19,28 @@ def fetch(suburb='Northcote'):
 
     suburb_json = {'_id': suburb}
     suburb_json['venues'] = []
+    suburb_unique_beers = {}
+    suburb_unique_beer_locations = {}
+
     for venue in venues:
         venue_json = venue
         checkins = untappd.get_venue_checkins(foursquare_venue_id=venue['id'])
         if not checkins:
             continue
         venue_json['checkins'] = checkins
+        venue_unique_beers = {}
+        for checkin in checkins:
+            bid = str(checkin['beer']['bid'])
+            venue_unique_beers[bid] = checkin['beer']
+            suburb_unique_beers[bid] = checkin['beer']
+            if not bid in  suburb_unique_beer_locations:
+                suburb_unique_beer_locations[bid] = []
+            suburb_unique_beer_locations[bid].append(venue)
+
+        venue_json['unique_beers'] = venue_unique_beers
         suburb_json['venues'].append(venue_json)
+        suburb_json['unique_beers'] = suburb_unique_beers
+        suburb_json['unique_beer_locations'] = suburb_unique_beer_locations
 
     mongo_client.hoponit.suburbs.insert(suburb_json)
     return suburb_json
